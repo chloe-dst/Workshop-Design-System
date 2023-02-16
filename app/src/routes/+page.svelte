562c84ae158d9d4ca625d247f7ec5b8666c3b515
ValentinR01 <valentin.rejaunier@hetic.net>
2023-02-16 16:31:48 +0100

    add input component

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import Text from '../components/UI/atoms/Text.svelte';
+    import Input from '../components/UI/atoms/Input.svelte';
     import '../assets/global.css';
 </script>
 
@@ -7,3 +8,9 @@
   tag='h2' style='title'>
   Yo Valentin
 </Text>
+
+<Input
+  type='text'
+  id='username'
+  name='username'
+  placeholder='Username'/>
